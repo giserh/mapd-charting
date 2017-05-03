0406ef9f548159437120e860c1afb288163d3598
Tai Dupree <tdupreetan@gmail.com>
2017-05-03 12:05:55 -0700

    fix test

--- src/mixins/label-mixin.unit.spec.js
+++ src/mixins/label-mixin.unit.spec.js
@@ -2,10 +2,17 @@ import mixin from "./label-mixin"
 import {expect} from "chai"
 
 
-describe("laebl mixin", () => {
+describe("label mixin", () => {
+  let chart
+
+  beforeEach(()=>{
+    chart = {
+      on: () => () => null,
+    }
+  })
   describe("measureLabelsOn", () => {
     it('should set and get _measureLabelsOn', () => {
-      const chart = mixin({})
+      chart = mixin(chart)
       expect(chart.measureLabelsOn()).to.equal(false)
       chart.measureLabelsOn(true)
       expect(chart.measureLabelsOn()).to.equal(true)
